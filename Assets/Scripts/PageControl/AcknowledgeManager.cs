7388fa2068ab786c930fb97350e85c6e4143f14f
Xinxin F <fanxinxin@outlook.com>
2024-03-31 15:19:12 +0800

    On Home page, add acknowledgement button, scene and also the quit button

--- Assets/Scripts/PageControl/AcknowledgeManager.cs
+++ Assets/Scripts/PageControl/AcknowledgeManager.cs
@@ -3,13 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuManager : MonoBehaviour
+public class AcknowledgeManager : MonoBehaviour
 {
-    public void ChangeScene(string name){
+    public void ReturnHome(string name){
         SceneManager.LoadScene(name);
-    }
-
-    public void QuitGame(){
-        Application.Quit();
     }
 }
