7a65f3c8352440e6aa7ec15cad2f1bdaef12f193
Xinxin F <fanxinxin@outlook.com>
2024-03-31 14:40:42 +0800

    Winning page can redirect to other scenes. There is no ranking list at the moment

--- Assets/Scripts/PageControl/WinningManager.cs
+++ Assets/Scripts/PageControl/WinningManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class WinningManager : MonoBehaviour
 {
+
+    
     public void SwitchScene(string name){
         SceneManager.LoadScene(name);
     }
